d8a46c4d74be6088d7f6bc964d2f0b046235c38e
yangxin <995249880@qq.com>
2026-09-22 10:50:01 +0800

    2026/09/22:#master6592Game:同步修改模式列表的写入方式,和列表该用ScrollView

--- app/src/main/java/com/twd/gamesetting/light/ModeSelectActivity.java
+++ app/src/main/java/com/twd/gamesetting/light/ModeSelectActivity.java
@@ -1,17 +1,14 @@
 package com.twd.gamesetting.light;
 
-import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.twd.gamesetting.R;
@@ -19,141 +16,138 @@ import com.twd.gamesetting.utils.SystemUtils;
 
 public class ModeSelectActivity extends AppCompatActivity {
 
-    private static final String JOYSTICK_LED_MODE = "persist.sys.joystick.led";
-    private static final String path_mode = "/sys/leds/mode";
+    private static final String JOYSTICK_LED_MODE  = "persist.mode.joystick.led";
+    private static final String JOYSTICK_LED_COLOR = "persist.color.joystick.led";
+    private static final String path_mode  = "/sys/leds/mode";
     private static final String path_color = "/sys/leds/color";
 
-    private String[] MODE_NAMES;
+    private static final int MODE_RAINBOW_ROTATE = 16;
 
-    // {path_mode, path_color}，path_color = -1 表示不写
     private static final int[][] MODE_NODE_MAP = {
-            {0, -1}, // 0  关闭
-            {1,  0}, // 1  长亮_红
-            {1,  1}, // 2  长亮_黄
-            {1,  2}, // 3  长亮_绿
-            {1,  3}, // 4  长亮_天空蓝
-            {1,  4}, // 5  长亮_蓝
-            {1,  5}, // 6  长亮_紫
-            {1,  6}, // 7  长亮_淡蓝
-            {2,  0}, // 8  呼吸_红
-            {2,  1}, // 9  呼吸_绿
-            {2,  2}, // 10 呼吸_蓝
-            {2,  3}, // 11 呼吸_紫
-            {2,  4}, // 12 呼吸_天空蓝
-            {2,  5}, // 13 呼吸_黄
-            {2,  6}, // 14 呼吸_淡蓝
-            {3, -1}, // 15 呼吸_变色循环
-            {4, -1}, // 16 炫彩_旋转
+            {0, -1}, {1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6},
+            {2, 0}, {2, 1}, {2, 2}, {2, 3}, {2, 4}, {2, 5}, {2, 6},
+            {3, -1}, {4, -1},
     };
 
-    private ListView lvMode;
-    private ModeItemAdapter modeItemAdapter;
-    private final Context context = this;
+    private LinearLayout llModeContainer;
+    private String[] MODE_NAMES;
+    private final View[] itemViews = new View[MODE_NODE_MAP.length];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode_select);
-        String MODE_OFF = getResources().getString(R.string.light_mode_turn_off);
-        String MODE_STATIC_RED = getResources().getString(R.string.light_mode_steady_on_Red);
-        String MODE_STATIC_YELLOW = getResources().getString(R.string.light_mode_steady_on_Yellow);
-        String MODE_STATIC_GREEN = getResources().getString(R.string.light_mode_steady_on_Green);
-        String MODE_STATIC_SKYBLUE = getResources().getString(R.string.light_mode_steady_on_Skyblue);
-        String MODE_STATIC_BLUE = getResources().getString(R.string.light_mode_steady_on_Blue);
-        String MODE_STATIC_PURPLE = getResources().getString(R.string.light_mode_steady_on_Purple);
-        String MODE_STATIC_LIGHTBLUE = getResources().getString(R.string.light_mode_steady_on_Lightblue);
-        String MODE_BREATH_RED = getResources().getString(R.string.light_mode_breath_Red);
-        String MODE_BREATH_YELLOW = getResources().getString(R.string.light_mode_breath_Yellow);
-        String MODE_BREATH_GREEN = getResources().getString(R.string.light_mode_breath_Green);
-        String MODE_BREATH_SKYBLUE = getResources().getString(R.string.light_mode_breath_Skyblue);
-        String MODE_BREATH_BLUE = getResources().getString(R.string.light_mode_breath_Blue);
-        String MODE_BREATH_PURPLE = getResources().getString(R.string.light_mode_breath_Purple);
-        String MODE_BREATH_LIGHTBLUE = getResources().getString(R.string.light_mode_breath_Lightblue);
-        String MODE_BREATH_COLORCIRCLE = getResources().getString(R.string.light_mode_breath_ColorCycle);
-        String MODE_rotating_COLORCIRCLE = getResources().getString(R.string.light_mode_rotating_ColorCycle);
+
+        initModeNames();
+
+        llModeContainer = findViewById(R.id.ll_mode_container);
+
+        // 动态添加每个选项
+        LayoutInflater inflater = LayoutInflater.from(this);
+        for (int i = 0; i < MODE_NAMES.length; i++) {
+            final int position = i;
+            View item = inflater.inflate(R.layout.item_mode, llModeContainer, false);
+            TextView tvName = item.findViewById(R.id.tv_name);
+            tvName.setText(MODE_NAMES[i]);
+
+            item.setOnClickListener(v -> {
+                setModeValue(position);
+                finish();
+            });
+
+            // 有些遥控器用 DPAD_CENTER 选中，有些用 ENTER；clickable + focusable 后
+            // 系统会自动把 DPAD_CENTER 转成 click，这里不用额外监听。
+            itemViews[i] = item;
+            llModeContainer.addView(item);
+        }
+
+        // 初始焦点定位到当前模式项
+        int kernelMode  = Integer.parseInt(SystemUtils.getProperty(JOYSTICK_LED_MODE, "0"));
+        int kernelColor = Integer.parseInt(SystemUtils.getProperty(JOYSTICK_LED_COLOR, "-1"));
+        int curPosition = modeKernelToIndex(kernelMode, kernelColor);
+
+        View target = itemViews[curPosition];
+        ScrollView sv = findViewById(R.id.sv_mode);
+        int parentWidth  = getResources().getDisplayMetrics().widthPixels;
+        int parentHeight = getResources().getDisplayMetrics().heightPixels;
+
+        int widthSpec  = View.MeasureSpec.makeMeasureSpec(parentWidth,  View.MeasureSpec.EXACTLY);
+        int heightSpec = View.MeasureSpec.makeMeasureSpec(parentHeight, View.MeasureSpec.AT_MOST);
+
+        sv.measure(widthSpec, heightSpec);
+        sv.layout(0, 0, sv.getMeasuredWidth(), sv.getMeasuredHeight());
+
+        target.requestFocus();
+        centerItemInScrollView(sv, target);
+    }
+
+    private void initModeNames() {
         MODE_NAMES = new String[]{
-                MODE_OFF, MODE_STATIC_RED, MODE_STATIC_YELLOW, MODE_STATIC_GREEN, MODE_STATIC_SKYBLUE,
-                MODE_STATIC_BLUE, MODE_STATIC_PURPLE, MODE_STATIC_LIGHTBLUE, MODE_BREATH_RED, MODE_BREATH_YELLOW,
-                MODE_BREATH_GREEN, MODE_BREATH_SKYBLUE, MODE_BREATH_BLUE, MODE_BREATH_PURPLE, MODE_BREATH_LIGHTBLUE,
-                MODE_BREATH_COLORCIRCLE, MODE_rotating_COLORCIRCLE
+                getString(R.string.light_mode_turn_off),
+                getString(R.string.light_mode_steady_on_Red),
+                getString(R.string.light_mode_steady_on_Green),
+                getString(R.string.light_mode_steady_on_Blue),
+                getString(R.string.light_mode_steady_on_Purple),
+                getString(R.string.light_mode_steady_on_Lightblue),
+                getString(R.string.light_mode_steady_on_Yellow),
+                getString(R.string.light_mode_steady_on_White),
+                getString(R.string.light_mode_breath_Red),
+                getString(R.string.light_mode_breath_Green),
+                getString(R.string.light_mode_breath_Blue),
+                getString(R.string.light_mode_breath_Purple),
+                getString(R.string.light_mode_breath_Lightblue),
+                getString(R.string.light_mode_breath_Yellow),
+                getString(R.string.light_mode_breath_White),
+                getString(R.string.light_mode_breath_ColorCycle),
+                getString(R.string.light_mode_rotating_ColorCycle),
         };
-        lvMode = findViewById(R.id.lv_mode);
-        modeItemAdapter = new ModeItemAdapter(context, MODE_NAMES);
-        lvMode.setAdapter(modeItemAdapter);
-
-        int cur = Integer.parseInt(SystemUtils.getProperty(JOYSTICK_LED_MODE, "0"));
-        lvMode.post(() -> lvMode.setSelection(cur));
-
-        lvMode.setOnItemClickListener((parent, view, position, id) -> {
-            setModeValue(position);
-            finish();
-        });
-
-        lvMode.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                modeItemAdapter.setFocusedItem(position);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
     }
 
-    private void setModeValue(int value) {
-        SystemUtils.setProperty(JOYSTICK_LED_MODE, String.valueOf(value));
+    private void setModeValue(int position) {
+        if (position < 0 || position >= MODE_NODE_MAP.length) return;
 
-        if (value < 0 || value >= MODE_NODE_MAP.length) return;
+        int[] nodes = MODE_NODE_MAP[position];
+        int kernelMode  = nodes[0];
+        int kernelColor = nodes[1];
 
-        int[] nodes = MODE_NODE_MAP[value];
-        SystemUtils.writeSysNode(path_mode, nodes[0]);
-        if (value == 16) {
-            SystemUtils.writeSysNode(path_mode, 3); // 先切到呼吸变色循环
-            SystemUtils.writeSysNode(path_mode, 4); // 再切到炫彩旋转
-            return;
-        }
+        SystemUtils.setProperty(JOYSTICK_LED_MODE,  String.valueOf(kernelMode));
+        SystemUtils.setProperty(JOYSTICK_LED_COLOR, String.valueOf(kernelColor));
 
-        SystemUtils.writeSysNode(path_mode, nodes[0]);
-        if (nodes[1] >= 0) {
-            SystemUtils.writeSysNode(path_color, nodes[1]);
+        if (position == MODE_RAINBOW_ROTATE) {
+            SystemUtils.writeSysNode(path_mode, 3);
+            SystemUtils.writeSysNode(path_mode, 4);
+        } else {
+            SystemUtils.writeSysNode(path_mode, kernelMode);
+            if (kernelColor >= 0) {
+                SystemUtils.writeSysNode(path_color, kernelColor);
+            }
         }
     }
 
-    class ModeItemAdapter extends ArrayAdapter<String> {
-        private LayoutInflater inflater;
-        private int focusedItem = 0;
-
-        public ModeItemAdapter(@NonNull Context context, String[] objects) {
-            super(context, 0, objects);
-            inflater = LayoutInflater.from(context);
-        }
-
-        public void setFocusedItem(int position) {
-            focusedItem = position;
-            notifyDataSetChanged();
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View itemView = convertView;
-            if (itemView == null) {
-                itemView = inflater.inflate(R.layout.item_mode, parent, false);
+    private int modeKernelToIndex(int kernelMode, int kernelColor) {
+        for (int i = 0; i < MODE_NODE_MAP.length; i++) {
+            int[] node = MODE_NODE_MAP[i];
+            if (node[0] == kernelMode && node[1] == kernelColor) {
+                return i;
             }
-
-            TextView tvName = itemView.findViewById(R.id.tv_name);
-            tvName.setText(getItem(position));
-
-            // 焦点效果
-            if (position == focusedItem) {
-                itemView.setBackgroundResource(R.color.bg_focus);
-                tvName.setTextColor(context.getResources().getColor(R.color.tv_focus));
-            } else {
-                itemView.setBackgroundResource(R.color.bg_normal);
-                tvName.setTextColor(context.getResources().getColor(R.color.tv_normal));
-            }
-
-            return itemView;
         }
+        return 0;
+    }
+
+    private void centerItemInScrollView(ScrollView sv, View target) {
+        int targetTop      = target.getTop();
+        int targetHeight   = target.getHeight();
+        int targetCenter   = targetTop + targetHeight / 2;
+
+        int viewportHeight = sv.getHeight()
+                - sv.getPaddingTop() - sv.getPaddingBottom();
+
+        int targetScrollY  = targetCenter - viewportHeight / 2;
+
+        int maxScroll = Math.max(0,
+                llModeContainer.getHeight() - viewportHeight);
+        targetScrollY = Math.max(0, Math.min(targetScrollY, maxScroll));
+
+        sv.scrollTo(0, targetScrollY);
     }
 }
